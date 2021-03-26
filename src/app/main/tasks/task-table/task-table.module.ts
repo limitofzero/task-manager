52ba439dd8b474b10c57ba0921d71b41d7f0a648
limitofzero <limitofzero@gmail.com>
2021-03-26 23:49:24 +0300

    feat(): get table data

--- src/app/main/tasks/task-table/task-table.module.ts
+++ src/app/main/tasks/task-table/task-table.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskTableComponent } from './task-table/task-table.component';
+import {TuiTableModule} from '@taiga-ui/addon-table';
 
 @NgModule({
   declarations: [TaskTableComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    TuiTableModule,
   ],
   exports: [
-    TaskTableComponent
+    TaskTableComponent,
   ],
 })
 export class TaskTableModule { }
